e634f6f9ef1529eadc3cebc1f5c9d18ca03d6ba3
demo-test <Shri_Rakesh_Ji@gmail.com>
2025-08-23 16:07:27 +0530

    package for configuration. Removed configuration as a subpackage of service.

--- src/main/java/com/myplantdiary/enterprise/configuration/SingletonRetofit.java
+++ src/main/java/com/myplantdiary/enterprise/configuration/SingletonRetofit.java
@@ -1,4 +1,4 @@
-package com.myplantdiary.enterprise.service.configuratiom;
+package com.myplantdiary.enterprise.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

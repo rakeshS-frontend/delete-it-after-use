846301274befd79369b591419cff21643270a69e
demo-test <Shri_Rakesh_Ji@gmail.com>
2025-08-24 18:00:27 +0530

    ci/cd changes:- 1.profile created to avoid jpa failing. 2 properties files . invoke test only 2.Added config.yml for circle ci.

--- src/main/java/com/myplantdiary/enterprise/dao/ISpecimenDAO.java
+++ src/main/java/com/myplantdiary/enterprise/dao/ISpecimenDAO.java
@@ -1,8 +1,9 @@
 package com.myplantdiary.enterprise.dao;
 
 import com.myplantdiary.enterprise.dto.Specimen;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.repository.CrudRepository;
-
+@Profile("dev")
 public interface ISpecimenDAO extends CrudRepository<Specimen,Integer> {
 
 }
